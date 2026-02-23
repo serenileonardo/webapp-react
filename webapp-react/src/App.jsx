82075fb1f35e460935545ab9a000bcaf63dc4c49
serenileonardo <serenileonardo.web@gmail.com>
2026-02-23 14:13:07 +0100

    feat: put layout and detaill page

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,2 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
 
+function App() {
+    return (
+        <BrowserRouter>
+            <Layout>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movies/:id" element={<MovieDetail />} />
+                </Routes>
+
+            </Layout>
+        </BrowserRouter>
+    );
+}
+
+export default App;
