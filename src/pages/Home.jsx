0ae454458d996099bfa501c0fc44d4944655835e
serenileonardo <serenileonardo.web@gmail.com>
2026-02-24 16:55:53 +0100

    feat api ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,33 @@
-export default function Home() {
-    return <h2>HOME FUNZIONA</h2>;
+import { useEffect, useState } from "react";
+
+function Home() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        fetch("http://localhost:3000/movies")
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch((error) => {
+                console.error("Errore nel recupero dei film:", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista Film</h1>
+
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>
+                        <strong>{movie.title}</strong> — {movie.director}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+export default Home;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,3 +1,34 @@
-export default function MovieDetail() {
-    return <h2>Film ID: {id}</h2>;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function MovieDetail() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/movies/" + id)
+            .then(res => res.json())
+            .then(data => setMovie(data));
+    }, [id]);
+
+    if (!movie) return <p>Caricamento...</p>;
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <p>{movie.director}</p>
+
+            <h2>Recensioni</h2>
+
+            <ul>
+                {movie.reviews.map(r => (
+                    <li key={review.id}>
+                        {review.author}: {review.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+export default MovieDetail;
